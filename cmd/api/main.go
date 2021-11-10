cc1f691f4118e8d428963cec6d9e1b13d48070ae
Yurii Bliuchak <zero.ukr@gmail.com>
2021-11-10 23:45:16 +0100

    cmd/api: create necessery routes
    routes:
    - get book by id
    - create book

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"time"
 
+	"github.com/bliuchak/golang-workshop/internal/api"
+	"github.com/bliuchak/golang-workshop/internal/platform/storage"
 	"github.com/go-chi/chi/v5"
 	"github.com/rs/zerolog"
 )
@@ -14,12 +16,22 @@ func main() {
 	zerolog.TimeFieldFormat = time.RFC3339Nano
 	l := zerolog.New(os.Stderr).With().Timestamp().Logger()
 
-	l.Info().Msg("start app aaa")
+	l.Info().Msg("start app")
+
+	db, err := storage.NewRedis("storage", "6379")
+	if err != nil {
+		l.Fatal().Err(err).Msg("redis init error")
+	}
+
+	rest := api.NewAPI(db, l)
 
 	r := chi.NewMux()
 	r.Get("/", func(rw http.ResponseWriter, r *http.Request) {
 		rw.Write([]byte("hello world"))
 	})
+
+	r.Get("/book/{id}", rest.GetBook())
+	r.Post("/book", rest.CreateBook())
 
 	srv := &http.Server{
 		Addr:    net.JoinHostPort("", "3000"),
